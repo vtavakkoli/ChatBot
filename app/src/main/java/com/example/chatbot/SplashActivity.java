c652477896dcbdbd4c54e1666ee810a054b22ddb
Vahid Tavakkoli <vtavakkoli@yahoo.com>
2026-06-09 14:01:04 +0200

    Add splash screen and app info menu

---
+++ app/src/main/java/com/example/chatbot/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.chatbot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final long SPLASH_DELAY_MS = 1600L;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.splashRoot), (view, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }, SPLASH_DELAY_MS);
+    }
+}
